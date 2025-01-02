f69910d7eff18b79005c45735ee8e622202465eb
dhanie <romadani.code@gmail.com>
2025-01-02 12:10:06 +0700

    magic Book final

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
             ),
           )),
       debugShowCheckedModeBanner: false,
-      home: const MainNavigationView(),
+      home: const SplashView(),
     );
   }
 }
